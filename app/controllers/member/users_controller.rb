de4f8f9f76115cf91616299081e3f2f7942e2b57
EC2 Default User <ec2-user@ip-172-31-19-10.ap-northeast-1.compute.internal>
2024-10-14 15:46:14 +0000

    マイページ編集

--- app/controllers/member/users_controller.rb
+++ app/controllers/member/users_controller.rb
@@ -1,9 +1,12 @@
 class Member::UsersController < ApplicationController
+  before_action :authenticate_customer!
   
   def edit
+    @user = current_user
   end 
   
   def show
+    @user = current_user
   end 
   
   def update
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   # 会員側
-  devise_for :users, controllers: {
+  devise_for :users, skip: [:passwords], controllers: {
     registrations: "member/registrations",
     sessions: 'member/sessions'
   }
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
 
     resources :comics, only: [:index, :create]
 
-    get "/users/:id/edit" => "users#edit", as: "edit_user"
+    get "/users/information/edit" => "users#edit", as: "edit_user"
     get "/users/my_page" => "users#show", as: "user"
     patch "/users/information" => "users#update", as: "update_user"
     patch "/users/withdraw" => "users#withdraw", as: "withdraw"
@@ -39,8 +39,5 @@ Rails.application.routes.draw do
     resources :comics, only: [:new, :index, :show, :edit, :update, :destroy]
 
   end
-
-
-
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
